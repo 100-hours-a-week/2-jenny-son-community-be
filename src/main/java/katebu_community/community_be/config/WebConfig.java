2912a08046028870260899ad7d64640c598a63a6
soonge2 <jiwonsoong13@gmail.com>
2025-03-26 02:21:51 +0900

    feat(web): CORS 매핑 및 업로드 리소스 핸들러 구현

---
+++ src/main/java/katebu_community/community_be/config/WebConfig.java
@@ -0,0 +1,24 @@
+package katebu_community.community_be.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
+    }
+}
